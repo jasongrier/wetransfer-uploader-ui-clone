3260b201ba8fcfa73f239a60ab4fe381ae28ba09
Jason Grier <jason@hemberlin.de>
2021-03-26 07:55:46 +0100

    No semicolons

--- src/file-uploader/components/App.tsx
+++ src/file-uploader/components/App.tsx
@@ -1,16 +1,16 @@
-import React, { StrictMode, ReactElement, useState, MutableRefObject } from "react";
-import { Provider } from "react-redux";
+import React, { StrictMode, ReactElement, useState, MutableRefObject } from "react"
+import { Provider } from "react-redux"
 
-import { store } from "../store";
+import { store } from "../store"
 
-import Box from "./Box";
-import Info from "./Info";
-import File from "./File";
-import Footer from "./Footer";
-import ProgressCircle from "./ProgressCircle";
+import Box from "./Box"
+import Info from "./Info"
+import File from "./File"
+import Footer from "./Footer"
+import ProgressCircle from "./ProgressCircle"
 
 export default (): ReactElement => {
-  const [barRef, setBarRef] = useState<MutableRefObject<SVGCircleElement | null>>();
+  const [barRef, setBarRef] = useState<MutableRefObject<SVGCircleElement | null>>()
 
   return (
     <StrictMode>
@@ -23,5 +23,5 @@ export default (): ReactElement => {
         <ProgressCircle getRef={(barRef) => setBarRef(barRef)} />
       </Box>
     </StrictMode>
-  );
-};
+  )
+}
